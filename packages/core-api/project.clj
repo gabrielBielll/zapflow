9bbce596b8672a8b47054af9977adbf45e6f6cde
gabrielBielll <gabrielbarbosa.ff@gmail.com>
2025-11-13 19:57:05 -0300

    Upgrade PostgreSQL dependency version
    Updated PostgreSQL dependency version from 42.2.10 to 42.7.3.

--- packages/core-api/project.clj
+++ packages/core-api/project.clj
@@ -11,7 +11,8 @@
                  [metosin/reitit "0.9.2"]
                  [cheshire "5.13.0"]
                  [clj-http "3.12.3"]
-                 [buddy/buddy-hashers "1.4.0"]]
+                 [buddy/buddy-hashers "1.4.0"]
+                 [org.postgresql/postgresql "42.7.3"]]
   :main ^:skip-aot core-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
